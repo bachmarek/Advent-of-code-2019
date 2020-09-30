5bca5fe3331ca3f7527a4ad93edb77859490432d
bachmarek <64902674+bachmarek@users.noreply.github.com>
2020-09-30 13:43:12 +0200

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,71 +1,47 @@
-from itertools import groupby
 first = 145852
 last = 616942
 
-def createList(r1, r2):  
-  res = [] 
-  while(r1 < r2+1): 
-    res.append(r1) 
-    r1 += 1
-  return res
+def PartOne(L):
+  n = [x for x in str(L)]
+  if (n[0] > n[1] or n[1] > n[2] or n[2] > n[3] or n[3] > n[4] or n[4] > n[5]):
+    return False
+  if (
+      not(n[0] == n[1] ) and
+      not(n[1] == n[2] and n[1] != n[0]) and
+      not(n[2] == n[3] and n[2] != n[1]) and
+      not(n[3] == n[4] and n[3] != n[2]) and
+      not(n[4] == n[5] and n[4])):
+    return False
+  else: 
+    return True
 
-input = createList(first,last)
+answer1 = 0
+num = first
+while num < last:
+  if PartOne(num) == True:
+    answer1 +=1
+  num +=1
 
-def ListOfStrings(input):
-  stringlist = []
-  for i in input:
-    x = str(i)
-    stringlist.append(x)
-  return stringlist
+def PartTwo(L):
+  n = [x for x in str(L)]
+  if (n[0] > n[1] or n[1] > n[2] or n[2] > n[3] or n[3] > n[4] or n[4] > n[5]):
+    return False
+  if (
+      not(n[0] == n[1] and n[0] != n[2]) and
+      not(n[1] == n[2] and n[1] != n[0] and n[1] != n[3]) and
+      not(n[2] == n[3] and n[2] != n[1] and n[2] != n[4]) and
+      not(n[3] == n[4] and n[3] != n[2] and n[3] != n[5]) and
+      not(n[4] == n[5] and n[4] != n[3])):
+    return False
+  else: 
+    return True
 
-inputX = ListOfStrings(input)
+answer2 = 0
+number = first
+while number < last:
+  if PartTwo(number) == True:
+    answer2 +=1
+  number +=1
 
-def Deconstruct(input):
-  deco_list = []
-  for obj in input:
-    deco_obj = []
-    deco_list.append(deco_obj)
-    for i in obj:
-      deco_obj.append(int(i))
-  return deco_list
-
-listXX = Deconstruct(inputX)
-
-def non_decreasing(L):
-  return all(x<=y for x, y in zip(L, L[1:]))
-
-def Decrease(L):
-  decrease_output = []
-  for i in range(len(L)):
-    if non_decreasing(L[i]) == True:
-      decrease_output.append(L[i])
-    else:
-      None
-  return decrease_output
-
-def same_numbers(L):
-  return any(x==y for x, y in zip(L, L[1:]))
-
-def Nextnumbers(L):
-  next_output = []
-  for i in range(len(L)):
-    if same_numbers(L[i]) == True:
-      next_output.append(L[i])
-    else:
-      None
-  return next_output
-
-result_one = Nextnumbers(decreased_list)
-decreased_list = Decrease(listXX)
-
-# not mine
-def valid_passwords(start, end, grp_len_pred):
-  for pwd in range(start, end + 1):
-    digits = [int(d) for d in str(pwd)]
-    if digits != sorted(digits): continue
-    if grp_len_pred({len(list(n)) for d, n in groupby(digits)}):
-      yield pwd
-
-if __name__ == "__main__":
-  valid2 = list(valid_passwords(first, last, lambda g: 2 in g))
-  print(f"Part 2: {len(list(valid2))}")
+print("Part 1: " + str(answer1))
+print("Part 2: " + str(answer2))
